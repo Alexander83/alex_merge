9460254b607ef48d6f239e9c4b0244747397aaf7
Alexander P <alex.podmasteriev@faceit.com.ua>
2014-07-03 15:51:59 +0300

    #1

---
+++ index2.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kop
+ * Date: 03.07.14
+ * Time: 15:51
+ * To change this template use File | Settings | File Templates.
+ */
+echo "file2";
+?>
